6b0c9c74f58c51251043b1a5e06ba7dcf2301c75
Rehmah Ahmed Batki <rehmahahmed1@gmail.com>
2026-07-18 21:22:19 +0530

    first commit

---
+++ service.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const expressLayout = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
+
+app.use(expressLayout);
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.set('layout', './layouts/layout');
+
+mongoose.connect('process.env.MONGODB_URI', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000');
+});
